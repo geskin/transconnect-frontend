ec970d21dbe7480473baf97d7866cab613dc714b
Ehretz Geskin <sg3584@alum.barnard.edu>
2025-03-13 16:25:06 -0400

    Trying to fix deployment issues

--- transconnect/vite.config.js
+++ transconnect/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
+// import { defineConfig } from 'vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    include: ['crypto-browserify'],
+  },
+  define: {
+    global: 'window', // or 'this'
+  },
+});
